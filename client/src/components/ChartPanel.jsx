d4005f09e3a6017b9b4d869c4e3bd0e837989830
Rashi-Dwivedi1812 <rashidwivedi1812@gmail.com>
2025-07-23 00:13:31 +0530

    Analyze page added

--- client/src/components/ChartPanel.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChartPanel = () => (
-  <div className="border rounded p-4">Chart Panel (placeholder)</div>
-);
-
-export default ChartPanel; 
--- client/src/components/LineChart.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LineChart = () => (
-  <div className="border rounded p-4">Line Chart (placeholder)</div>
-);
-
-export default LineChart; 
