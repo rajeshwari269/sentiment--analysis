d071e5e2cd093dc7239b118a13c12fa62ae63c5e
Mounika Macharla <mounikamacharla333@gmail.com>
2025-07-27 13:38:09 +0530

    Adds forgot-reset password functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -182,12 +182,20 @@ const Navbar = () => {
             </NavLink>
           )}
           {token && (
-            <button
-              onClick={logout}
-              className="px-4 py-2 text-red-600 border border-red-600 rounded hover:bg-red-50 transition"
-            >
-              Logout
-            </button>
+            <>
+              <NavLink
+                to="/forgot-password"
+                className="px-4 py-2 text-green-600 border border-green-600 rounded hover:bg-green-50 transition"
+              >
+                Reset Password
+              </NavLink>
+              <button
+                onClick={logout}
+                className="px-4 py-2 text-red-600 border border-red-600 rounded hover:bg-red-50 transition"
+              >
+                Logout
+              </button>
+            </>
           )}
         </div>
       </div>
