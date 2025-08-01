6c9ab24246d5838af91712aa0a912c74bd8e932e
Vivek Prakash <vivekprakash.st@gmail.com>
2025-08-02 00:51:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-// https://vite.dev/config/
-
-=======
->>>>>>> ed6ba4dea6940acd2b80ea9373c462f64e12a09b
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
